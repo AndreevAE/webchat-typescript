e4728935d1aae6e65ca1eb4a5119711bc068183f
Alexander Andreev <andreev_ae@bw-sw.com>
2016-12-07 17:23:52 +0700

    configure editor, eslint, package, edit gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-warning-comments": [
+            1,
+            {
+                "terms": [
+                    "todo"
+                ],
+                "location": "anywhere"
+            }
+        ]
+    },
+};
